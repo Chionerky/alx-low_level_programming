bbbdb908cb571956f1c5fa5db299fa15a53a56d4
chibuike <chibuikeaugustine147@gmail.com>
2022-09-10 09:53:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#inlude <stdio.h>
+
+
+/**
+* main - print the string in the put function
+* this program prints "programming is like building multilingual puzzles
+* Return; 0
+*/
+int main(void)
+{
+      puts(*\"Programming is like building a multilingual puzzle");
+      Return (0);
+}
