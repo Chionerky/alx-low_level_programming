db7d95c591eeab20cfc57b06787b5b86834b4c51
chibuike <chibuikeaugustine147@gmail.com>
2022-09-11 08:23:56 -0700

    commit task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-
-#include <studio.h>
+#include <stdio.h>
 
 /**
-* main - prints the  exactly "Programming is like building a multilingual puzzle
-* Description: using the main function\
-
-* Return 0; (sucessful)
-*/
+ * main - prints the string in the pus function
+ *
+ * description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return 0; (sucessful) 
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
