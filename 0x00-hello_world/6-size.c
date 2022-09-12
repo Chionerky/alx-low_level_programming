77aaa747fa5639c349cf0a1ca907f6ee41a8fd94
chibuike <chibuikeaugustine147@gmail.com>
2022-09-11 20:31:20 -0700

    hold

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,21 +3,21 @@
 /**
 * main -Entry point
 *
-* Description "Programming is like building a multilingual puzzle
+* Description "prints exactly with proper grammar
 * Return: Always 0 (sucess)
 */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
-	printf("Size of a char: %d byte(s)\n", sizeof(a));
-	printf("Size of an int: %d byte(s)\n", sizeof(b));
-	printf("Size of a long int: %d byte(s)\n", sizeof(c));
-	printf("Size of long long int: %d byte(s)\n", sizeof(d));
-	printf("Size of a float: %d byte(s)\n", sizeof(e));
- 	return (0);
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Siza of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ldbyte(s)\n", sizeof(lli));
+	printf("Size of a float: %ldbyte(s)\n", sizeof(f));
+	return (0);
 }
