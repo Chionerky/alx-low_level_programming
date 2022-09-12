1297561cecdaa087b6207a3689ead3a68f6cf2a8
chibuike <chibuikeaugustine147@gmail.com>
2022-09-11 19:48:40 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main -Entry point
+*
+* Description "Programming is like building a multilingual puzzle
+* Return: Always 0 (sucess)
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+ 	return (0);
+}
