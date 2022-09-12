93cfcd0a4c97f0399002eb8092f5007a82503730
chibuike <chibuikeaugustine147@gmail.com>
2022-09-11 17:14:13 -0700

    brtty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the function
- *
- * Description: using the main function
- * this prints "Programming is like building a multilingual puzzle
- * Return: Always 0 (successful)
- */
+* main - print the string in the function
+*
+* Description: using the main function
+* this prints "Programming is like building a multilingual puzzle
+* Return: Always 0 (successful)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
