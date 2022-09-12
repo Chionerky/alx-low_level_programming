1e2c2475d20a37dd560b76a2ef160277881a9fa1
chibuike <chibuikeaugustine147@gmail.com>
2022-09-11 18:49:26 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#inlude <stdio.h>
+
+/**
+* main -Entry point
+*
+* Description prints exactly with proper grammar
+*
+* Return: Always 0 (Sucessful)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
