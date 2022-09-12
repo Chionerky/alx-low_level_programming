40b010c776ef5425117cebca1d3b25551b3af674
chibuike <chibuikeaugustine147@gmail.com>
2022-09-11 17:56:33 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the function
-*
-* Description: using the main function
-* this prints "Programming is like building a multilingual puzzle
-* Return: Always 0 (successful)
+* main - prints
+* 
+* Return: 0 Sucessful
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
-}
-
