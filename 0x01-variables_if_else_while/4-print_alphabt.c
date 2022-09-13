3c8269ac783880519532de5b9dcf217a47a6a0d3
chibuike <chibuikeaugustine147@gmail.com>
2022-09-12 17:04:14 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Prints the lowercase letters 
+* i e, q and e
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e'; && a != 'q')
+			putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+		
