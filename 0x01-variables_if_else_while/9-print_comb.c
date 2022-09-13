e01cb5d8b03d8f88f614e956854e1a90102bc730
chibuike <chibuikeaugustine147@gmail.com>
2022-09-12 19:00:17 -0700

    print-comb0

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - prints combinations of single digits
+*
+* Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+
+
+	putchar('\n');
+
+	return (0);
+}
+
