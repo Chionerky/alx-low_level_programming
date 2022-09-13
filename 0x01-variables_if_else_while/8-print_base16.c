039dff97f48f83fd3c2443805109bf4f9c357626
chibuike <chibuikeaugustine147@gmail.com>
2022-09-12 18:33:52 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Prints all the base 16 as folloe
+*
+* REturn:0
+*/
+int main(void)
+{
+	int num;
+	char a;
+
+	for (num = 0; num < 10; num++)
+		putchar(num % 10) + '0');
+
+	for (a = 'a'; a <= 'f'; a++)
+		puthcar(a);
+
+	putchar('\n');
+
+	return  (0);
+}
+
