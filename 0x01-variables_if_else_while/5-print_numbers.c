6595fcfde3a91a121d0e9a8158a0b1e41df5d0d4
chibuike <chibuikeaugustine147@gmail.com>
2022-09-12 17:29:18 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Prints all single digits of
+* base 10 start from 0
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
+
