ee15d50a0ae2ab0b753b82919a71292520781f8d
chibuike <chibuikeaugustine147@gmail.com>
2022-09-14 06:37:57 -0700

    10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print - print_alphabet_x10 ten -letters
+*
+* Return: Success
+*/
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char k;
+
+	while (count++ <=9 )
+	{
+		for (k = 'a'; k <= 'z'; k++)
+			_putchar(k);
+
+		_putchar('\n');
+	}
+}
+
