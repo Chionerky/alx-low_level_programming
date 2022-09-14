1464f2bb100c60204a467019cd2ec8881f353f36
chionerky <chibuikeaugustine147@gmail.com>
2022-09-14 11:54:07 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_to_98 - prints all natural nums input-98
+*	in order by separeted comma, folloed by space
+*@n: the number to begin with
+*/
+void print_to_98(int n)
+{
+	if  (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
+
+
