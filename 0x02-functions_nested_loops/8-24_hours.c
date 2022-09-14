ce4595024d35067fe0b516f9f385f3fd62b972ed
chionerky <chibuikeaugustine147@gmail.com>
2022-09-14 09:47:01 -0700

    agent Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* jack_bauer - Prints every mins of the day
+*		jack bauer, 24hrs day
+*/
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minutes = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
