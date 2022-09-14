254149c53770e0e847c27d625f0f6813ec0ec201
chionerky <chibuikeaugustine147@gmail.com>
2022-09-14 15:43:37 -0700

    multiply nums

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ void print_times_table(int n)
 		{
 			_putchar('0');
 
-			for (mult =1; mult <= n; mult++)
+			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -31,7 +31,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
