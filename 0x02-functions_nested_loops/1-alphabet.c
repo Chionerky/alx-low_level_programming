7aa9864c1152648f05c81a742f545743bd1fd263
chibuike <chibuikeaugustine147@gmail.com>
2022-09-14 05:35:25 -0700

    the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* print_alphabet - functions to print abc
+*
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+
+}
+
