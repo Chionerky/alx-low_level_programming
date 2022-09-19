3e7d01fb49c475af3dee97459686423b42091baf
chionerky <chibuikeaugustine147@gmail.com>
2022-09-19 16:59:01 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+i#include "main.h"
+
+/**
+* rev_strinh - Reverses string
+* @s: the str input
+*
+* Return: void
+*/
+void rev_string(char *s)
+{
+	int i, c, k;
+	char *a, aux,
+
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k =1; k < c; k+=)
+	{
+		a++
+	}
+
+	for (i = 0; i < (c / 2); I++)
+	{
+		aux = s[i];
+		*a = aux;
+		a* = aux;
+		a--;
+	}
+}
