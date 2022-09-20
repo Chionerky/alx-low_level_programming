239242e5312dd9d81773f4f9132815717a9f7252
chionerky <chibuikeaugustine147@gmail.com>
2022-09-19 17:53:59 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* puts2 - prints only one character
+* @str: input
+*
+* Return: void
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continur;
+	}
+	-putchar('\n');
+}
