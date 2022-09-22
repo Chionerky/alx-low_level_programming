2fca5c952a1f38f9ee68321dc15e5b27fb52b5e9
chionerky <chibuikeaugustine147@gmail.com>
2022-09-21 19:20:01 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcat - concentrates two strings
+* @dest: destination of the string
+* @src: source of string
+*
+* Return: A pointer to the string results
+*/
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; scr[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+code to Write a function that concatenates two strings.
+
+using strncat Write a function that concatenates two strings.
+
+Write a function that copies a string.
+
+Write a function that compares two strings.
