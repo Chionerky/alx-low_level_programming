d46ee08c0c3b404a54c9670837672bbcb74cbcf6
chionerky <chibuikeaugustine147@gmail.com>
2022-09-23 08:49:31 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
 /**
-* cap_string - capitalizes all words of a strong
-* @s: string
+* cap_string - funcions that cap[italizes first character of word
+* @str: string input to cpitalize
 *
-* Return: Capitalized string
+* Return: returns the value
 */
-
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 44, 59, 46, 33, 63, 34, 63, 40, 41, 123, 125,};
+	int index = 0;
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-
-	while (*(s + count) != '\0')
+	while (str[++index])
 	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
 
-				break;
-			}
-		}
-		count++;
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			str[index] -= 32;
 	}
-
-	return (s);
+	return (str);
 }
