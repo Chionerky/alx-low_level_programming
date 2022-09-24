f70860a5e3d47bd73a2ca17b217d637363c7f3e9
chionerky <chibuikeaugustine147@gmail.com>
2022-09-23 19:52:48 -0700

    finished

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,7 @@
 * Return: the pointer to dest
 */
 
-char cap_string(char *s)
+char *cap_string(char *s)
 {
 	int count = 0, i;
 	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
