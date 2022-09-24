cabf8d05ce212bc54a56f14e402b6d71b3bb83ad
chionerky <chibuikeaugustine147@gmail.com>
2022-09-23 20:48:11 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* rot13 - encodes a string into rot13
+* @s: string input
+* Return: address of s
+*/
+
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = bb[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
