9737ac5e6a43241827732c906751c7cc58ad665c
chionerky <chibuikeaugustine147@gmail.com>
2022-09-27 21:39:05 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* prints_string - sets the value of ptr toa set
+* @s: pointer to ptr
+* @to: pointer to char
+*
+* Return: void
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
