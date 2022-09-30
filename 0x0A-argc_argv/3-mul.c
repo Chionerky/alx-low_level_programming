3989534d463e56b2c07e40017e6400ca77505d37
chionerky <chibuikeaugustine147@gmail.com>
2022-09-30 11:06:01 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: arg count 
+ * @argv: arg vector
+ *
+ * Return: 0;
+ */
+int main(int argc, char argv[])
+{
+	int index, multiplication;
+
+	multiplication = 1;
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
+}
