8e25beed34a51215bad6fe3dd3c3431dc1bfd054
chionerky <chibuikeaugustine147@gmail.com>
2022-09-30 10:25:02 -0700

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - write program to prints name, followed by new line
+ * rename the program to print new name
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+Write a program that prints the number of arguments passed into it.
+Your program should print a number, followed by a new line
+A-argc argv
