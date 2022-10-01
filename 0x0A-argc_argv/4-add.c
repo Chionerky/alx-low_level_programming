4ee210baece47b0eb99afb6d3763ba1b89da115e
chionerky <chibuikeaugustine147@gmail.com>
2022-09-30 21:52:51 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <type.h>
+#include <ctype.h>
 /**
  * main - Adds positive number
  * @argc: arg count
@@ -11,6 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0, i;
+
 	if (argc > 1)
 	{
 		for  (i = 1; i < argc; i++)
@@ -19,7 +20,8 @@ int main(int argc, char *argv[])
 			char *str;
 
 			str = argv[i];
-			for (b =0; str[b] != '\0'; b++)
+
+			for (b = 0; str[b] != '\0'; b++)
 			{
 				if (str[b] < 48 || str[b] > 57)
 				{
