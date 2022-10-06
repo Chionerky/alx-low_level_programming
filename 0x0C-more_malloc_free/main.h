b4129c98028fb7d8a6da51956a3086bfd5a3f01d
chionerky <chibuikeaugustine147@gmail.com>
2022-10-06 04:16:54 -0700

    malloc prnt

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts(char *str);
+int _putchar(char);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+
+#endif
