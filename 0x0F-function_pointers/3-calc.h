c01eb46b5e6707ff9f8f0f948987376b10af5325
chionerky <chibuikeaugustine147@gmail.com>
2022-10-12 15:50:15 -0700

    done task

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
 #ifndef CALC_H
-#define CALC_h
+#define CALC_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,7 +13,7 @@
 typedef struct op
 {
 	char *op;
-	int (*f)(int a, intb);
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
