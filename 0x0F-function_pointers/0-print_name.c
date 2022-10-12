f1827dfd143bce85129a2098d03ba15c7a75ccba
chionerky <chibuikeaugustine147@gmail.com>
2022-10-12 08:09:47 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
 /**
- * print_name - prints a name
- * @name: name to be printed
- *f: Function pointer
- *Return: void
- */
+* print_name - prints a name
+* @name: name to be printed
+* f: Function pointer
+*
+* Return: void
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
